8fedeee234798a43a88e2f2495c1040ebf0ede2a
Jay Chalmers <jamiechalmers@live.co.uk>
2022-03-27 18:49:40 +0100

    Fix twilio, add basic message

--- Countdown.cs
+++ Countdown.cs
@@ -3,11 +3,14 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Twilio;
+using Twilio.Rest.Api.V2010.Account;
 
 namespace Minnoq.Countdown
 {
     public class Countdown
     {
+        private readonly DateTime WeddingDate = new DateTime(2023, 08, 08);
+
         [FunctionName("Countdown")]
         public void Run([TimerTrigger("0 0 7 * * *")]TimerInfo myTimer, ILogger log)
         {
@@ -16,13 +19,19 @@ namespace Minnoq.Countdown
             string fromNumber = Environment.GetEnvironmentVariable("FROM_NUMBER");
             string toNumber = Environment.GetEnvironmentVariable("TO_NUMBER");
 
-            TwilioClient.init(accountSid, authToken);
+            TwilioClient.Init(accountSid, authToken);
 
             var message = MessageResource.Create(
-                body: "This is a test message from the app",
+                body: GetBody(),
                 from: new Twilio.Types.PhoneNumber(fromNumber),
-                toNumber: new Twilio.Types.PhoneNumber(toNumber)
+                to: new Twilio.Types.PhoneNumber(toNumber)
             );
+        }
+
+        private string GetBody()
+        {
+            var now = DateTime.UtcNow;
+            return $"It's {WeddingDate.Subtract(now).Days} days til our wedding!";
         }
     }
 }
