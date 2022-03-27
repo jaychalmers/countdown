aa6d2d8762b20b45718de299d2c3a94f8adefcf4
Jay Chalmers <jamiechalmers@live.co.uk>
2022-03-27 18:27:59 +0100

    Add twilio and test message

--- Countdown.cs
+++ Countdown.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Twilio;
 
 namespace Minnoq.Countdown
 {
@@ -10,7 +11,18 @@ namespace Minnoq.Countdown
         [FunctionName("Countdown")]
         public void Run([TimerTrigger("0 0 7 * * *")]TimerInfo myTimer, ILogger log)
         {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+            string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+            string fromNumber = Environment.GetEnvironmentVariable("FROM_NUMBER");
+            string toNumber = Environment.GetEnvironmentVariable("TO_NUMBER");
+
+            TwilioClient.init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "This is a test message from the app",
+                from: new Twilio.Types.PhoneNumber(fromNumber),
+                toNumber: new Twilio.Types.PhoneNumber(toNumber)
+            );
         }
     }
 }
